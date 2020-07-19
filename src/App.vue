c2ca291c5ccd93907f20586f20a14d8d6fff742e
AdmiralSoviet <gogodaddy27@gmail.com>
2020-07-19 18:00:33 +1000

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="out-out-wrap">
+    <div id="content" tabindex="0">
+      <MainMenu />
+    </div>
+    <div id="notif-section"></div>
+    <footer>
+      <span>&#169;Magic Dice 2020</span>
+      <span>
+        A tool created by
+        <a href="https://nikgo.me" target="_blank">Nikita Golev</a>
+      </span>
+      <span>
+        Contact me by
+        <a href="mailto:ngolev.bus@gmail.com">Email</a>
+      </span>
+      <span>
+        Github
+        <a href="https://nikgo.me" target="_blank">Source Code</a>
+      </span>
+      <span>
+        Full Credits
+      </span>
+    </footer>
+  </div>
+</template>
+
+<script>
+import MainMenu from "./components/MainMenu.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainMenu
+  }
+};
+</script>
+
+<style>
+@import "./assets/css/style.css";
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <transition appear name="animate__fadeInDown">
+    <div id="main-wrap">
+      <img src="../assets/images/MagicLogo.png" id="MagicDiceLogo" alt="Magic Dice Logo" />
+      <div id="main-menu">
+        <span class="menu-option" id="menu-rolldice">Roll Dice</span>
+        <span class="menu-option" onclick="MagicUI.createCharacter()">Create Character</span>
+        <span class="menu-option" id="menu-load">Load Character</span>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "MainMenu"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../assets/css/animate.min.css";
+#main-wrap {
+  text-align: center;
+  top: 0;
+  bottom: 100px;
+  margin: auto;
+  position: absolute;
+  height: max-content;
+  left: 0;
+  right: 0;
+}
+
+#MagicDiceLogo {
+  display: block;
+  max-width: 450px;
+  margin: auto;
+}
+
+#main-menu {
+  vertical-align: middle;
+  text-align: center;
+  background: none;
+  box-shadow: none;
+}
+
+.menu-option {
+  text-shadow: 0px 0px 3px black;
+  border: dotted 2px white;
+  border-radius: 3px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.menu-option:hover {
+  background: none;
+  color: #c1cace;
+  border-color: #c1cace;
+  font-weight: normal;
+}
+</style>
